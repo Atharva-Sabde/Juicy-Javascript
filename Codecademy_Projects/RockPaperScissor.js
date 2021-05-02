e245158fbf48f7eb9c4c106aaad47d575458e56d
Atharva Sabde <atharvaa.sabde@gmail.com>
2021-05-02 21:17:25 +0530

    gameV.1.0

--- Codecademy_Projects/RockPaperScissor.js
+++ Codecademy_Projects/RockPaperScissor.js
@@ -11,7 +11,14 @@ if(userInput === 'rock' || userInput === 'scissors' || userInput==='paper'){
 // console.log(getUserChoice(''));
 const getComputerChoice = () => {
   var x = (Math.floor(Math.random() * 3))
-  console.log(x)
+  if(x == 1){
+    return 'rock'
+  }
+  else if(x == 2){
+    return 'paper'
+  }else{
+    return 'scissors'
+  }
 }
 
 const determineWinner = (userChoice , computerChoice) => {
@@ -19,7 +26,6 @@ const determineWinner = (userChoice , computerChoice) => {
     return 'tie , go fuck yourself bitch!'
   }
   else{
-
     if(userChoice == 'rock'){
       if(computerChoice== 'paper'){
         return 'Computer Won!'
@@ -43,4 +49,19 @@ const determineWinner = (userChoice , computerChoice) => {
     }
   }
   }   // method close
+
+//;
+
+function playGame(){
+  userChoice = getUserChoice('rock');
+  computerChoice = getComputerChoice();
+
+  console.log(userChoice)
+  console.log(computerChoice)
+ console.log(determineWinner(userChoice , computerChoice))
+}
+
+
+playGame()
+
 
